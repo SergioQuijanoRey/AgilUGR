a73112906e694bb1532caed177db49f78e5ddae8
Sergio <sergiquijano@gmail.com>
2021-10-15 19:44:18 +0200

    feat #10: añado mas posibilidades para los eventos que podemos manejar

--- app/src/main/java/com/project/agilugr/gestures/Gestures.kt
+++ app/src/main/java/com/project/agilugr/gestures/Gestures.kt
@@ -1,7 +1,11 @@
 package com.project.agilugr.gestures
 
+import android.os.Build
+import android.view.GestureDetector
 import android.view.MotionEvent
 import androidx.activity.ComponentActivity
+import androidx.annotation.RequiresApi
+import androidx.core.view.GestureDetectorCompat
 import androidx.core.view.MotionEventCompat
 import com.project.agilugr.ui.navigation.NavigationDirector
 import com.project.agilugr.ui.navigation.NavigationMapper
@@ -9,8 +13,26 @@ import com.project.agilugr.ui.navigation.NavigationMapper
 /** Interfaz que define lo que debe implementar un manager de gestos */
 interface GestureManager{
 
-    /** Procesa un evento sobre la pantalla del dispositivo */
+    /** Procesa un evento simple sobre la pantalla del dispositivo */
     fun onTouch(event: MotionEvent): Boolean
+
+    /**
+     * Gestiona el evento de cuando ponemos el dedo sobre la pantalla. Casi todos los eventos
+     * procesan este evento en primer lugar, asi que no puede ser bloqueado
+     * */
+    fun onDown(event: MotionEvent): Boolean
+
+    /** Evento que se lanza cuando dejamos pulsado sobre la pantalla durante un tiempo largo */
+    fun onLongPress(event: MotionEvent)
+
+    /** Evento que se lanza cuando hacemos dos toques seguidos en la pantalla */
+    fun onDoubleTap(event: MotionEvent): Boolean
+
+    /**
+     * TODO -- No se cual es la intencion de esta funcion pero necesitamos implementarla para
+     * que funcione la herencia que luego hacemos
+     * */
+    fun onShowPress(event: MotionEvent)
 }
 
 /**
@@ -18,15 +40,27 @@ interface GestureManager{
  * @param navigationDirector para poder navegar a los distintos puntos de nuestra aplicacion
  *
  * Notar que tambien podemos gestionar los eventos a nivel de cada componente de Jetpack Compose
+ *
+ * TODO -- la interfaz recoge los metodos que tenemos que implementar por las clases abstractas
+ * de las que heredamos
  * */
-class DefaultGestureManager(val navigationDirector: NavigationDirector): GestureManager, ComponentActivity()  {
+@RequiresApi(Build.VERSION_CODES.M)
+class DefaultGestureManager(val navigationDirector: NavigationDirector):
+    ComponentActivity(),
+    GestureDetector.OnGestureListener,
+    GestureDetector.OnDoubleTapListener,
+    GestureDetector.OnContextClickListener,
+    GestureManager
+{
 
     /**
-     *  Define como gestionamos los eventos de dedos sobre la pantalla
+     * Define como gestionamos los eventos de dedos sobre la pantalla
+     * Solo tenemos que gestionar los eventos que no gestionamos por otras funciones que
+     * no sean onTouch
      *
      * @param event el evento que tenemos que procesar
      * */
-    override fun onTouch(event: MotionEvent): Boolean{
+    override fun onTouch(event: MotionEvent): Boolean {
         val action: Int = MotionEventCompat.getActionMasked(event)
 
         return when (action) {
@@ -48,5 +82,50 @@ class DefaultGestureManager(val navigationDirector: NavigationDirector): Gesture
             }
             else -> super.onTouchEvent(event)
         }
+    }
+
+    override fun onDown(event: MotionEvent): Boolean {
+        // No hacemos nada con el evento, devolvemos true para no bloquear el resto de eventos
+        // que usan este evento como partida
+        return true
+    }
+
+    override fun onShowPress(event: MotionEvent) {
+        // No hacemos nada
+    }
+
+    override fun onSingleTapUp(p0: MotionEvent?): Boolean {
+        return true
+    }
+
+    override fun onScroll(p0: MotionEvent?, p1: MotionEvent?, p2: Float, p3: Float): Boolean {
+        return true
+    }
+
+    /** Evento que se lanza cuando dejamos pulsado sobre la pantalla durante un tiempo largo */
+    override fun onLongPress(event: MotionEvent) {
+        // No hacemos nada de momento
+    }
+
+    override fun onFling(p0: MotionEvent?, p1: MotionEvent?, p2: Float, p3: Float): Boolean {
+        return true
+    }
+
+    override fun onSingleTapConfirmed(p0: MotionEvent?): Boolean {
+        return true
+    }
+
+    /** Evento que se lanza cuando hacemos dos toques seguidos en la pantalla */
+    override fun onDoubleTap(event: MotionEvent): Boolean {
+        this.navigationDirector.navigate(NavigationMapper.FOCUS_MODE_SESSION)
+        return true
+    }
+
+    override fun onDoubleTapEvent(p0: MotionEvent?): Boolean {
+        return true
+    }
+
+    override fun onContextClick(p0: MotionEvent?): Boolean {
+        return true
     }
 }
