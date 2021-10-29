27d0cd07ab462f8d3f74fa59116d93fcc355dfbe
Sergio <sergiquijano@gmail.com>
2021-10-29 12:24:49 +0200

    test #29: testeo tambien que lanzamos excepciones cuando toca

--- app/src/test/java/com/project/agilugr/ui/navigation/NavigationMapperTest.kt
+++ app/src/test/java/com/project/agilugr/ui/navigation/NavigationMapperTest.kt
@@ -3,6 +3,7 @@ package com.project.agilugr.ui.navigation
 import org.junit.Assert.*
 
 import org.junit.Test
+import java.lang.Exception
 
 class NavigationMapperTest {
 
@@ -12,5 +13,17 @@ class NavigationMapperTest {
         for(value in NavigationMapper.values()){
             assertTrue("No se mappea bien la ruta al enumerado", value == stringToEnum(value.route))
         }
+    }
+
+    /** Comprobamos que se lanza una excepcion para valores que no tengan sentido*/
+    @Test
+    fun test_string_to_enum_fails_in_some_cases(){
+        var badRoute = "route_no_valida"
+
+        val exception = assertThrows(Exception::class.java) {
+            val enumValue = stringToEnum(badRoute)
+        }
+        assertEquals("La ruta $badRoute no es valida", exception.message)
+
     }
 }
